56aa9ff714c0ba6405d2fa411ecffdb6273ffbfc
Irene <icn2104@columbia.edu>
2014-04-08 12:59:38 -0400

    I've updated my javascript file

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
  * @author Chidinma Irene Nwoye
  */
 
+var video = "<iframe width='560' height='315' src='http://www.youtube.com/embed/9evIG1MugW0?html5=1' frameborder='0' allowfullscreen></iframe>";
+
 $(document).ready(function(){
 	setNav();
 });
@@ -10,7 +12,10 @@ function setNav(){
 	
 	$(".cyclone").on("click", function(){
 		$('#myModal').modal();
+		$('.modal-body').html(video);
 	});
 	
-	
+	$(".close").on("click", function(){
+		$(".modal-body").empty();
+		});
 }
