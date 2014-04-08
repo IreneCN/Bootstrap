92320eb164a54f0fca8b75165afd1241f542dcf3
Irene <icn2104@columbia.edu>
2014-04-08 12:15:32 -0400

    This is my javascript file for working with bootstrap

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ * @author Chidinma Irene Nwoye
+ */
+
+$(document).ready(function(){
+	setNav();
+});
+
+function setNav(){
+	
+	$(".cyclone").on("click", function(){
+		$('#myModal').modal();
+	});
+	
+	
+}
